481d1c6306b4610bfbd9560400d7f44117001b58
Brenda Iñiguez <brenda@Brendas-MacBook-Air.local>
2016-05-30 20:31:34 -0500

    Initial Commit

---
+++ GoT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoT
+//
+//  Created by Brenda Iñiguez on 5/30/16.
+//  Copyright (c) 2016 Brenda Iñiguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GoT/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GoT
+//
+//  Created by Brenda Iñiguez on 5/30/16.
+//  Copyright (c) 2016 Brenda Iñiguez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
