6da521a78c845ccf976bf58779ac79814c010cb8
Brenda Iñiguez <brenda@Brendas-MacBook-Air.local>
2016-06-06 21:44:29 -0500

    added new page

--- GoT/Score.h
+++ GoT/Score.h
@@ -1,6 +1,6 @@
 //
 //  Score.h
-//  GoT
+//  GoT/Users/brenda/Documents/GoT App/GoT/GoT/Score.m
 //
 //  Created by Brenda Iñiguez on 6/3/16.
 //  Copyright (c) 2016 Brenda Iñiguez. All rights reserved.
